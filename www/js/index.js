a88343aca1b11c47dbd20d295cb1c3437d5e5900
kechkibet <kech@ol-digital.com>
2015-04-02 13:43:27 +0300

    7

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
-        $("#btncontacts").click(function(e){
+        $("#btncontacts").click(function(){
             app.getContact();
         });
     },
